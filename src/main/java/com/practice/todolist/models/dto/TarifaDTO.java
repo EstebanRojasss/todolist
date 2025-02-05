7e9a71d26a5294a184d6d6f370dd698a89ccde26
Esteban Rojas <estebanrojasch.2@gmail.com>
2025-02-04 21:14:14 -0300

    Creacion de de DTOS y entidades

--- src/main/java/com/practice/todolist/models/dto/TarifaDTO.java
+++ src/main/java/com/practice/todolist/models/dto/TarifaDTO.java
@@ -1,2 +1,30 @@
-package com.practice.todolist.models.dto;public record TarifaDTO() {
+package com.practice.todolist.models.dto;
+
+import lombok.Getter;
+
+
+public record TarifaDTO(
+                        String nombre,
+                        Integer prioridad,
+                        Boolean realizado,
+                        String descripcion) {
+    @Override
+    public String nombre() {
+        return nombre;
+    }
+
+    @Override
+    public Integer prioridad() {
+        return prioridad;
+    }
+
+    @Override
+    public Boolean realizado() {
+        return realizado;
+    }
+
+    @Override
+    public String descripcion() {
+        return descripcion;
+    }
 }
--- src/main/java/com/practice/todolist/models/entities/Tarifa.java
+++ src/main/java/com/practice/todolist/models/entities/Tarifa.java
@@ -1,2 +1,20 @@
-package com.practice.todolist.models.entities;public class Tarifa {
+package com.practice.todolist.models.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Tarifa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private Integer prioridad;
+    private Boolean realizado;
+    private String descripcion;
+
 }
