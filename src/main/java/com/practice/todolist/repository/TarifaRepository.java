2f2f46eef7e5f6ec77191060c26edf533788b349
Esteban Rojas <estebanrojasch.2@gmail.com>
2025-02-04 21:19:01 -0300

    Creacion del repositorio, controller, servicio

---
+++ src/main/java/com/practice/todolist/repository/TarifaRepository.java
@@ -0,0 +1,8 @@
+package com.practice.todolist.repository;
+
+import com.practice.todolist.models.entities.Tarifa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TarifaRepository extends JpaRepository<Tarifa, Long> {
+
+}
